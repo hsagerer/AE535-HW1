6e508f0cee77fdc045eee405894642115a99d093
cohelsel <cohelsel@umich.edu>
2022-10-03 22:41:19 -0400

    Update lunarRendPayload.m

--- lunarRendPayload.m
+++ lunarRendPayload.m
@@ -1,5 +1,16 @@
 function [] = lunarRendPayload()
 % Function for finding the payload mass in a direct ascent trajectory   
 
+Isp = 311;
+g0 = 9.81; 
+
+mf1 = 11900;
+deltaV1 = 0.9;
+
+mf2 = 11900 + 4280;
+deltaV2 = 0.1 + 2.2 + 2.5;
+
+mpLOR1 = mf1/exp(-deltaV1/(Isp*g0));
+mpLOR2 = mf2/exp(-deltaV2/(Isp*g0));
 
 end
