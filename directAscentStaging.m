9203d13e6621213e054b3c75ccd8f2b1e75bb041
cohelsel <cohelsel@umich.edu>
2022-10-04 13:17:12 -0400

    Update directAscentStaging.m

--- directAscentStaging.m
+++ directAscentStaging.m
@@ -8,8 +8,12 @@ mL = directAscentPayload();
 g0 = 9.81;
 deltaV3 = 3200;
 Isp3 = 421;
+Eps3 = 0.19;
 
-mStage3 = mL/exp(-deltaV3/(Isp3*g0));
+R3 = exp(deltaV3/(Isp3*g0));
+
+Gam3 = (1-Eps3*R3)/(R3-1);
+
 
 
 end
