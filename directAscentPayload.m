941589ddea417eca62f93c59d5a50dda5d5ab342
cohelsel <cohelsel@umich.edu>
2022-10-03 22:31:35 -0400

    Update directAscentPayload.m

--- directAscentPayload.m
+++ directAscentPayload.m
@@ -6,8 +6,8 @@ deltaV = 0.9 + 2.2 + 2.5 + 0.9; % deltaV of CSM, DA
 Isp = 311; % ISP of Aerozine-50
 g0 = 9.81; 
 
-mpDA = mf*(exp(deltaV/(Isp*g0))-1);
+mpDA = mf/exp(-deltaV/(Isp*g0));
 
-fprintf('Mass of something: %0.3f kg\n',mpDA);
+fprintf('Payload Mass of Direct Ascent: %0.3f kg\n',mpDA);
 
 end
