c51b3474a91cab80c083a2ab975c8658b2642ed1
cohelsel <cohelsel@umich.edu>
2022-10-04 17:34:11 -0400

    Update directAscentStaging.m

--- directAscentStaging.m
+++ directAscentStaging.m
@@ -14,7 +14,19 @@ Isp = [283,311,421];
 Epsilon = [0.05,0.07,0.19];
 
 for i = 1:3
-    eqn = Isp(i)*g0*ln((a*Isp(i)*g0 + 1)/(a*Isp(i)*g0*Epsilon(i)));
+    eqn(i) = Isp(i)*g0*log((a*Isp(i)*g0 + 1)/(a*Isp(i)*g0*Epsilon(i)));
 end
+
+EQN = eqn(1) + eqn(2) + eqn(3);
+
+alpha = double(solve(EQN,a))
+
+R = (alpha.*Isp.*g0 + 1)./(alpha.*Isp.*g0.*Epsilon)
+
+figure();
+plot(-1:0.03:1,subs(EQN,a,-1:0.03:1));
+hold on;
+plot([-1,1],[deltaV,deltaV]);
+hold off;
 
 end
