7e5ffa15a3eba58972d8235c395953e117978429
cohelsel <cohelsel@umich.edu>
2022-10-04 12:33:05 -0400

    Update directAscentStaging.m

--- directAscentStaging.m
+++ directAscentStaging.m
@@ -4,7 +4,12 @@ function [] = directAscentStaging()
 
 % Mass of third stage 
 
-directAscentPayload();
+mL = directAscentPayload();
+g0 = 9.81;
+deltaV3 = 3200;
+Isp3 = 421;
+
+mStage3 = mL/exp(-deltaV3/(Isp3*g0));
 
 
 end
