64057fd568185c0c66b8df538742a1331d6cca43
cohelsel <cohelsel@umich.edu>
2022-10-03 22:23:15 -0400

    Update directAscentPayload.m

--- directAscentPayload.m
+++ directAscentPayload.m
@@ -1,5 +1,11 @@
 function [] = directAscentPayload()
 % Function for finding the payload mass in a direct ascent trajectory   
 
+mf = 11900; % CSM dry mass
+deltaV = 0.9 + 2.2 + 2.5 + 0.9; % deltaV of CSM, DA
+Isp = 311; % ISP of Aerozine-50
+g0 = 9.81; 
+
+mpDA = mf*(exp(deltaV/(Isp*g0))-1);
 
 end
