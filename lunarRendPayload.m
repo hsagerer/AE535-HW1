b5a363d9c1e55d551af9d09688e7462d2e91625a
cohelsel <cohelsel@umich.edu>
2022-10-03 23:04:05 -0400

    LOR

--- lunarRendPayload.m
+++ lunarRendPayload.m
@@ -7,17 +7,17 @@ g0 = 9.81;
 mf1 = 11900;
 deltaV1 = 0.9;
 
-mf2 = 11900 + 4280;
+mf2 = 4280;
 deltaV2 = 0.1 + 2.2 + 2.5;
 
 deltaV3 = 0.9;
 
-mpLOR1 = mf1/exp(-deltaV1/(Isp*g0));
-mpLOR2 = (mpLOR1+4280)/exp(-deltaV2/(Isp*g0));
-mpLOR3 = mpLOR2/exp(-deltaV3/(Isp*g0));
+moLOR1 = mf1/exp(-deltaV1/(Isp*g0));
+moLOR2 = (4280)/exp(-deltaV2/(Isp*g0));
+moLOR3 = (moLOR2 + moLOR1)/exp(-deltaV3/(Isp*g0));
 
-fprintf('Payload Mass of LOR1: %0.3f kg\n',mpLOR1);
-fprintf('Payload Mass of LOR2: %0.3f kg\n',mpLOR2);
-fprintf('Payload Mass of LOR3: %0.3f kg\n',mpLOR3);
+fprintf('Payload Mass of LOR1: %0.3f kg\n',moLOR1);
+fprintf('Payload Mass of LOR2: %0.3f kg\n',moLOR2);
+fprintf('Payload Mass of LOR3: %0.3f kg\n',moLOR3);
 
 end
